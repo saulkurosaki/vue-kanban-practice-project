fd5ef3f2726a99bad90b54eb07a6cef148a271e4
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-14 10:52:30 -0600

    feat(InputNew): implement the form, input, input ref, along the handleSubmit function initial layout

---
+++ src/components/InputNew.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "@vue/reactivity";
+
+const text = ref();
+
+function handleSubmit() {
+  if (text !== "") {
+    //mandar a llamar el emit
+  }
+}
+</script>
+
+<template>
+  <form action="" @submit.prevent="handleSubmit">
+    <input type="text" v-bind="text" />
+  </form>
+</template>
+
+<style scoped></style>
